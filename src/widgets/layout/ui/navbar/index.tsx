3d7aa5972f780adfa961b574563c121a501c0609
Alisher <zumanovaliser814@gmail.com>
2026-03-07 17:59:19 +0600

    da

--- src/widgets/layout/ui/navbar/index.tsx
+++ src/widgets/layout/ui/navbar/index.tsx
@@ -2,7 +2,7 @@ import { NAVBAR_ITEMS } from "@/shared/constants";
 
 export const Navbar = () => {
   return (
-    <nav className="bg-black p-5 sticky top-0 z-10">
+    <nav className="bg-[#004C97] p-5 sticky top-0 z-10">
       <ul className="max-w-400 m-auto flex items-center justify-evenly gap-5 px-10 text-white font-semibold text-lg">
         {NAVBAR_ITEMS.map((item) => (
           <li key={item}>{item}</li>
