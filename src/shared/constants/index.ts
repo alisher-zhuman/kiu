f551e711df231e8fd60e86fc7ce01b92bc5968a7
Alisher <zumanovaliser814@gmail.com>
2026-03-07 17:57:47 +0600

    da

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,2 +1,3 @@
 export * from "./locale";
 export * from "./metadata";
+export * from "./navbar";
--- src/widgets/layout/ui/navbar/index.tsx
+++ src/widgets/layout/ui/navbar/index.tsx
@@ -1,13 +1,12 @@
+import { NAVBAR_ITEMS } from "@/shared/constants";
+
 export const Navbar = () => {
   return (
     <nav className="bg-black p-5 sticky top-0 z-10">
       <ul className="max-w-400 m-auto flex items-center justify-evenly gap-5 px-10 text-white font-semibold text-lg">
-        <li>Абитуриенттерге</li>
-        <li>Абитуриенттерге</li>
-        <li>Абитуриенттерге</li>
-        <li>Абитуриенттерге</li>
-        <li>Абитуриенттерге</li>
-        <li>Абитуриенттерге</li>
+        {NAVBAR_ITEMS.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </nav>
   );
