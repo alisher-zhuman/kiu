89159b0e29df0680e4872225d1c1badae102f967
Alisher <zumanovaliser814@gmail.com>
2026-04-03 17:04:18 +0600

    save

--- src/widgets/applicants/ui/support-card/index.tsx
+++ src/widgets/applicants/ui/support-card/index.tsx
@@ -11,7 +11,7 @@ export const SupportCard = ({ text }: Props) => (
         <HandCoins size={42} strokeWidth={1.75} />
       </div>
 
-      <p className="text-base leading-7 text-black/85 md:text-[1.7rem] md:leading-[1.5]">
+      <p className="text-base leading-7 text-black/85 md:text-[1.7rem] md:leading-normal">
         {text}
       </p>
     </div>
