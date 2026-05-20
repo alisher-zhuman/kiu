74df94cfcf257643696e02317c0621811535defc
Alisher <zumanovaliser814@gmail.com>
2026-05-20 11:04:38 +0600

    feat: add DOCUMENTS_BY_TYPE api route constant

--- src/shared/constants/api-routes.ts
+++ src/shared/constants/api-routes.ts
@@ -1,6 +1,7 @@
 export const API_ROUTES = {
   AUTH_LOG_IN: "/auth/login",
   DOCUMENTS: "/documents",
+  DOCUMENTS_BY_TYPE: "/documents/by-type",
   IMAGES_DELETE: "/images/delete",
   IMAGES_UPLOAD: "/images/upload",
   NEWS: "/news",
